349b8ba1bc54b5df954c3bd63a1d511b19d5f23f
Erick Lino <eri.sion@live.com>
2021-05-02 19:12:35 -0500

    Security

--- copygram/copygram/settings.py
+++ copygram/copygram/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*s!0fqq%=qq=ftk)u7(b7ggnyp^bky=4kghgxzt(3fv87%^0sv'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
