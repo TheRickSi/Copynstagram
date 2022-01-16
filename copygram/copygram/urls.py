3fbab54e8c498403c83bca6c8023bc5ca905e3bd
TheRicksi <eri.sion@live.com>
2022-01-15 19:30:14 -0500

    Manejo de templates

--- copygram/copygram/urls.py
+++ copygram/copygram/urls.py
@@ -15,10 +15,12 @@ urlpatterns = [
 
     path('admin/', admin.site.urls),
 
-    path('posts/', posts_views.list_posts, name='feed'),
+    path('', posts_views.list_posts, name='feed'),
+    path('post/new', posts_views.create_post, name ='new_post'),
 
     path('users/login/', users_views.login_view, name='login'),
     path('users/logout/', users_views.logout_view, name='logout'),
     path('users/signup/', users_views.signup, name='signup'),
     path('users/me/profile', users_views.update_profile, name='update_profile')
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
