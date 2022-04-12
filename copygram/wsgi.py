aa986493aa12618035314bf6ef6e59c72c620738
TheRicksi <eri.sion@live.com>
2022-04-11 19:27:17 -0500

    DESPLIEGUE

--- copygram/wsgi.py
+++ copygram/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "platzigram.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "copygram.settings")
 
 application = get_wsgi_application()
