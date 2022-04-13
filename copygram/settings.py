ebe9a2e04cc170c10d89caf8de4db5e14ffd5a8e
TheRicksi <eri.sion@live.com>
2022-04-12 20:38:59 -0500

    CAMBIOS

--- copygram/settings.py
+++ copygram/settings.py
@@ -133,7 +133,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/app/staticfiles'
 STATIC_ROOT= os.path.join(BASE_DIR,'staticfiles')
 STATIC_FILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATICFILES_DIRS = (
